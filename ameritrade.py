9d9148c7299725d7aa3884c1b53f07bf48249bb9
LCSO-developer <doug.clelland@lcso.org>
2022-09-20 00:20:56 -0400

    watch list reading

--- ameritrade.py
+++ ameritrade.py
@@ -1,23 +1,89 @@
 import os, credentials, requests
 
 class api:
+    # we have to init with obtaining a temporary access token, using our refresh token
     token = None
 
+    # default auth headers, using a Bearer {{token}}
+    auth = {}
+
+    # static ameritrade account identifiers pulled from env, for ease of reference
+    client_id = None
+    account_id = None
+
+    # live account info, read from api
+    account_info_data = None
+    watch_lists_data = None
+    # focused watchlist
+    watch_list = []
+
+
     def __init__(this):
-        auth_config = {'client_id': os.getenv('ameritrade_client_id'), 'grant_type': 'refresh_token', 'refresh_token': os.getenv('refresh_token')}
-        r = requests.post('https://api.tdameritrade.com/v1/oauth2/token', data=auth_config)
-        if (r.status_code == 200):
-            this.token = r.json().get('access_token')
+        # set local version of ameritrade account info
+        this.account_id = os.getenv('ameritrade_account_id')
+        this.client_id = os.getenv('ameritrade_client_id')
+
+        # prepare access token request
+        auth_config = {'client_id': this.client_id, 'grant_type': 'refresh_token', 'refresh_token': os.getenv('refresh_token')}
+        q = requests.post('https://api.tdameritrade.com/v1/oauth2/token', data=auth_config)
+        if (q.status_code == 200):
+            this.token = q.json().get('access_token')
+            this.auth = {'Authorization': "Bearer {}".format(this.token)}
+            # get basic account info
+            this.account_info()
             # print(this.token)
         else:
-            print('connection issue')
+            print('issue obtaining access token')
 
     def account_info(this):
-        headers = {'Authorization': "Bearer {}".format(this.token)}
-        q = requests.get("https://api.tdameritrade.com/v1/accounts/{}".format(os.getenv('ameritrade_account_id')), headers=headers)
-        print(q.status_code)
-        print(q.json())
-        return q.json()
+        # return account info if we already have it
+        if this.account_info_data:
+            return this.account_info_data
+
+        # obtain account info via api
+        q = requests.get("https://api.tdameritrade.com/v1/accounts/{account_id}".format(account_id = this.account_id), headers=this.auth)
+        # print(q.status_code)
+        
+        if q.status_code == 200:
+            # print(q.json())
+            this.account_info_data = q.json()
+            return this.account_info_data
+            # return this # for chaining
+        else: 
+            print('issue obtaining account info')
+            return False
+
+    def watch_lists(this, id = None):
+        # id is an optional argument to request a specific watchlist, otherwise we just get all watchlists on the account in a list
+        q = requests.get("https://api.tdameritrade.com/v1/accounts/{account_id}/watchlists".format(account_id = this.account_id), headers=this.auth)
+        
+        if q.status_code == 200:
+            # print(q.json())
+            this.watch_lists_data = q.json()
+
+            # set watch list as simplified list of watch list items
+            this.watch_list = []
+            for item in this.watch_lists_data[0].get('watchlistItems'):
+                this.watch_list.append(item.get('instrument').get('symbol'))
+
+            return this.watch_list
+            # return this # for chaining
+        else: 
+            print('issue obtaining watch lists')
+            return False
+    
+    def quotes(this, symbols = []):
+        # symbols is a list of the symbols we want to get the price quotes for
+
+        # concat array to string, comma-separated list
+        symbol_list = ','.join(symbols)
+
+        q = requests.get("https://api.tdameritrade.com/v1/marketdata/quotes?symbol={symbol_list}".format(symbol_list = symbol_list), headers=this.auth)
+        if q.status_code == 200:
+            print(q.json())
+        else: 
+            print('issue obtaining stock quotes')
+            return False
 
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":    
@@ -26,3 +92,5 @@ if __name__ == "__main__":
     am = api()
     # print(am.token)
     print(am.account_info())
+    print(am.watch_lists())
+    print(am.quotes(am.watch_list))
